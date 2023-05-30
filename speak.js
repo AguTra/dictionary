1d9b65db79636cfa2fbf980e66e5d422a33e063b
Traico <agutra@gmail.com>
2023-05-29 23:33:32 -0300

    a lot of changes

--- speak.js
+++ speak.js
@@ -1,58 +1,33 @@
-
-
-//##############################################
-// const texts = document.querySelector(".texts");
-
-// window.SpeechRecognition =
-//     window.SpeechRecognition || window.webkitSpeechRecognition;
-
-// const recognition = new SpeechRecognition();
-// recognition.interimResults = true;
-
-// let p = document.createElement("p");
-
-
-// recognition.addEventListener("result", (e) => {
-//     p.classList.add("from-text");
-//     texts.appendChild(p);
-//     const text = Array.from(e.results)
-//         .map((result) => result[0])
-//         .map((result) => result.transcript)
-//         .join("");
-
-//     p.innerText = text;
-//     if (e.results[0].isFinal) {
-//         p = document.createElement("p");
-//     }
-//     $("#chatTextBox").val(e.results[0][0].transcript);
-// });
-
-// recognition.addEventListener("end", () => {
-//     recognition.start();
-// });
-
-// recognition.start();
-
-
-////
-
 function startDictation() {
     if (window.hasOwnProperty('webkitSpeechRecognition')) {
         var recognition = new webkitSpeechRecognition();
-//
+        //
         recognition.continuous = false;
         recognition.interimResults = false;
         recognition.lang = document.querySelector("#select_dialect").value;
         recognition.start();
-    
+        trear = document.getElementById('probando-sobreescribir').innerText = 'Listening'
+        const note = document.querySelector('.sobreescribir-color');
+        note.style.backgroundColor = 'red';
+
+
+
         recognition.onresult = function (e) {
             document.getElementById('transcript').value = e.results[0][0].transcript;
             recognition.stop();
+            back = document.getElementById('probando-sobreescribir').innerText = 'Translate text'
+            const note = document.querySelector('.sobreescribir-color');
+            note.style.backgroundColor = 'blue';
             document.getElementById('labnol').submit();
         };
 
         recognition.onerror = function (e) {
+            edit = document.getElementById('probando-sobreescribir').innerText = 'Translate text'
+            const note = document.querySelector('.sobreescribir-color');
+            note.style.backgroundColor = 'blue';
             recognition.stop();
         };
     }
 }
+
+
